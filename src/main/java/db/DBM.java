934d81868084c36499a05aad0909a0fe87d098a8
gadzik <glina41@o2.pl>
2020-12-01 00:52:45 +0100

    cutting off

--- src/main/java/db/DBM.java
+++
@@ -1,7 +0,0 @@
-package db;
-
-/**
- * Created by gadzik on 28.12.19.
- */
-public class DBM extends DB {
-}
--- src/main/java/osoba/Osoba_MocnaJednostka.java
+++
@@ -1,7 +0,0 @@
-package osoba;
-
-/**
- * Created by gadzik on 15.01.20.
- */
-public class Osoba_MocnaJednostka extends Osoba {
-}
